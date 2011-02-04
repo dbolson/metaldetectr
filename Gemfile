cfd9633e7e4b9199e8effc54f6ee609466f81061
Danny <dbolson@gmail.com>
2011-02-03 21:04:41 -0800

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'mysql2'
+gem 'haml'
+gem 'mechanize'
+gem 'will_paginate', '~> 3.0.pre2'
+gem 'jquery-rails'
+gem 'fastercsv'
+gem 'devise'
+
+group :test do
+  gem 'rspec-rails'
+  gem 'factory_girl'
+  gem 'rcov'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metaldetectr::Application.config.secret_token = '888906990e990cb947ccb191760e9a0911730391f97e1ca7db3f2056e13a53fd01bee1dabaa307aa4243811def9406eeab4ad831e75a03e629b9202c19bd0007'
