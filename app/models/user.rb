28510dc223402d4e9612332b6b3690f312eecfae
Danny <dbolson@gmail.com>
2011-06-18 11:49:03 -0700

    Allow user update without current password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :lastfm_username
 
   # Default state is a user with only read access.
   # Admin state is a user with read and write access.
@@ -21,4 +21,17 @@ class User < ActiveRecord::Base
       transition :admin => :default
     end
   end  
+
+  # Override devise method so the user can update his information without
+  # entering his current password.
+  def update_with_password(params={})
+    if params[:password].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    end
+
+    result = update_attributes(params)
+    clean_up_passwords
+    result
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe User do
-  context "has a state" do
-    it "should start as 'default'" do
+  describe "has a state" do
+    it "starts as 'default'" do
       user = Factory(:user)
       user.should be_default
     end
 
     context "that changes to adminstrator" do
-      it "should change" do
+      it "changes" do
         user = Factory(:user)
         user.adminify
         user.should be_admin
@@ -16,10 +16,100 @@ describe User do
     end
 
     context "that changes from administrator" do
-      it "should change" do
+      it "changes" do
         user = Factory(:user, :state => 'admin')
         user.deadminify
         user.should be_default
+      end
+    end
+  end
+
+  describe "updating" do
+    let(:user) { Factory(:user) }
+
+    context "with a new email" do
+      it "updates" do
+        user.update_attributes(:email => 'new@email.com')
+        user.reload.email.should == 'new@email.com'
+      end
+    end
+
+    context "with a password" do
+      it "updates" do
+        encrypted_password = user.encrypted_password
+        user.update_attributes(:password => 'newpass', :password_confirmation => 'newpass')
+        user.reload.encrypted_password.should_not == encrypted_password
+      end
+    end
+
+    context "with remember_me" do
+      it "updates" do
+        user.update_attributes(:remember_me => 1)
+        user.reload.remember_me.should == 1
+      end
+    end
+
+    context "with a lastfm_username" do
+      it "updates" do
+        user.update_attributes(:lastfm_username => 'foo')
+        user.reload.lastfm_username.should == 'foo'
+      end
+    end
+  end
+
+  describe "#update_with_password" do
+    let(:user) { Factory(:user) }
+
+    context "without a new password" do
+      it "updates" do
+        params = { :email => 'foo@bar.com' }
+        user.email.should_not == 'foo@bar.com'
+        user.update_with_password(params).should be_true
+        user.email.should == 'foo@bar.com'
+      end
+    end
+
+    context "with a new password" do
+      context "that is valid" do
+        it "updates" do
+          params = { :email => 'foo@bar.com', :password => 'newpass', :password_confirmation => 'newpass' }
+          user.email.should_not == 'foo@bar.com'
+          user.update_with_password(params)
+          user.reload.email.should == 'foo@bar.com'
+        end
+      end
+
+      context "that is invalid" do
+        before do
+          @params = { :email => 'foo', :password => 'newpass', :password_confirmation => 'badpass' }
+        end
+
+        it "sets errors" do
+          user.update_with_password(@params)
+          user.errors[:email].should_not be_nil
+          user.errors[:password].should_not be_nil
+        end
+
+        it "is false" do
+          user.update_with_password(@params).should be_false
+          user.reload.email.should_not == 'foo@bar.com'
+        end
+      end
+
+      context "with a set password and confirmation" do
+        before do
+          @params = { :password => 'newpass', :password_confirmation => 'newpass' }
+        end
+
+        it "clears out the password" do
+          user.update_with_password(@params)
+          user.password.should be_nil
+        end
+
+        it "clears out the password confirmation" do
+          user.update_with_password(@params)
+          user.password_confirmation.should be_nil
+        end
       end
     end
   end
